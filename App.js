98c4d1e31f8f90caf466648643e8341cd9cf9842
suraj kumar <surajcse24@gmail.com>
2024-01-18 14:55:24 +0530

    Inception

---
+++ App.js
@@ -0,0 +1,14 @@
+{/* <div id="parent">
+        <div id="child">
+            <h1>i am Groot</h1>
+            <h2>i am Groot 2</h2>
+        </div>
+        <div id="child2">
+            <h1>i am Groot</h1>
+            <h2>i am Groot 2</h2>
+        </div>
+    </div> */}
+    const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }), [React.createElement("h1", {}, "I am Groot"), React.createElement("h2", {}, "I am Groot 2")], React.createElement("div", { id: "child2" }),[React.createElement("h1", {}, "I am Groot"), React.createElement("h2", {}, "I am Groot 2")]])
+    const heading = React.createElement("h1", { id: "heading" }, "keep Hello from react !!");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
