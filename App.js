eecf5d52d3c27d8ab084f51b7bbc7f7b501b2cc7
suraj kumar <surajcse24@gmail.com>
2024-01-24 23:37:16 +0530

    Igniting our App

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-{/* <div id="parent">
+import React from "react";
+import ReactDOM  from "react-dom";
+/* <div id="parent">
         <div id="child">
             <h1>i am Groot</h1>
             <h2>i am Groot 2</h2>
@@ -7,8 +9,10 @@
             <h1>i am Groot</h1>
             <h2>i am Groot 2</h2>
         </div>
-    </div> */}
-    const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }), [React.createElement("h1", {}, "I am Groot"), React.createElement("h2", {}, "I am Groot 2")], React.createElement("div", { id: "child2" }),[React.createElement("h1", {}, "I am Groot"), React.createElement("h2", {}, "I am Groot 2")]])
-    const heading = React.createElement("h1", { id: "heading" }, "keep Hello from react !!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+    </div> */
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child1" }), [React.createElement("h1", {}, "I am Groot"), React.createElement("h2", {}, "I am Groot 2")],
+    React.createElement("div", { id: "child2" }), [React.createElement("h1", {}, "I am Groot"), React.createElement("h2", {}, "I am Groot 2")]])
+const heading = React.createElement("h1", { id: "heading" }, "keep Hello from react !!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # nameste-react
+
+#parcel
+-Dev Build
+-local server
+-HRM - Hot Module Replecement
+-File watching algrothim - written in c++
+-caching - for faster builds
+-Image optimisation
+-minification
+-bundling
+-compressing
+-consistent hosting
+-code splitting
+-Differential bundling - support older Browsers
+-Diagnostic 
+-Error handling
+-It gives us a way to host the App on https instead of htpp
+-Tree Shaking - remove unused Code
+-Different DEV and Production Bundles
